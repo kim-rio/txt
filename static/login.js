687f366f6b22783210d4e3dd0568ec4653283680
panky <kimariopancras10@gmail.com>
2025-08-13 06:35:24 +0300

    new version

--- static/login.js
+++ static/login.js
@@ -2,11 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('loginForm');
 
   form.addEventListener('submit', function (e) {
-    e.preventDefault(); // Stop the browser's default submit popup
+    e.preventDefault(); 
 
-    // You can do validation here if you want
-
-    // Submit form programmatically after preventing default
     form.submit();
   });
 });
